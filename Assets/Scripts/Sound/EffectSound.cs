46fe2b6d1f11c035f7d62684e34b696d6d78ac8e
Lee_JungYeon <lofl.lotus.flower@gmail.com>
2023-01-10 14:36:50 +0900

    Setting

---
+++ Assets/Scripts/Sound/EffectSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSound : MonoBehaviour
+{
+    public SoundData data;
+    public float volume {
+        get { return audioSource.volume; }
+        set { audioSource.volume = value; }
+    }
+    public bool mute {
+        get { return audioSource.mute; }
+        set { audioSource.mute = value; }
+    }
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = data.effectScale;
+        audioSource.mute = data.isMute;
+    }
+}
